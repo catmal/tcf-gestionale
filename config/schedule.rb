a842b3b538c1f4939769d377a5236292907bfb91
Mirco <mirco.cattabriga@gmail.com>
2024-03-09 23:31:18 +0100

    Added to scheduler db:sessions:trim

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,3 +21,7 @@
 every :hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
   rake 'ActiveRecord::SessionStore::Session.where(["updated_at < ?", 1.hour.ago]).delete_all'
 end
+
+every :day do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+  rake 'db:sessions:trim'
+end
