2e4731a51c60b1b56a935d00059915153cf19b0b
Mirco <mirco.cattabriga@gmail.com>
2024-03-22 04:54:00 +0100

    Auto increment number for sales order

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -1,4 +1,5 @@
 class SalesOrder < Order
   has_many :sales_order_lines, class_name: 'SalesOrderLine', foreign_key: 'order_id', dependent: :destroy
   has_many :purchase_orders, as: :sourceable
+  auto_increment :number, initial: "OC-#{Date.today.year}-0001"
 end
