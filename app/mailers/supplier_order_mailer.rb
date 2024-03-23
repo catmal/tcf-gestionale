7fdb19abe0d6544d00275beeb7a32f044a0454ff
Mirco <mirco.cattabriga@gmail.com>
2024-03-23 20:02:30 +0100

    Removed supplier order mailer

--- app/mailers/supplier_order_mailer.rb
+++
@@ -1,7 +0,0 @@
-class SupplierOrderMailer < ApplicationMailer
-  def new_supplier_order_email
-    @supplier_order = params[:supplier_order]
-
-    mail(to: @supplier_order.supplier.email, subject: 'Nuovo Ordine da TCF')
-  end
-end
