321c99406694fde513ba1f803ac33e939a43ae04
Mirco <mirco.cattabriga@gmail.com>
2024-03-23 18:16:38 +0100

    Fixed menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,11 +2,11 @@ class Menu < ApplicationRecord
   ITEMS = [
     { name: 'Distinte', icon: 'fa-brands fa-dochub', link: '/bill_of_materials',
       controller: 'bill_of_materials' },
-    { name: 'Articoli', icon: 'fa-solid fa-gear', link: '/items', controller: 'items' },
+    { name: 'Articoli', icon: 'fa-solid fa-barcode', link: '/items', controller: 'items' },
     { name: 'Fornitori', icon: 'fa-solid fa-users', link: '/suppliers', controller: 'suppliers' },
-    { name: 'Ordini Clienti', icon: 'fa-solid fa-truck', link: '/sales_orders',
+    { name: 'Ordini Clienti', icon: 'fa-solid fa-file', link: '/sales_orders',
       controller: 'sales_orders' },
-    { name: 'Ordini Fornitori', icon: 'fa-solid fa-truck', link: '/purchase_orders',
+    { name: 'Ordini Fornitori', icon: 'fa-solid fa-file', link: '/purchase_orders',
       controller: 'purchase_orders' }
 
   ].freeze
