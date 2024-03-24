f655b7059326347d75256f8163cf41e8731ea0b8
Mirco <mirco.cattabriga@gmail.com>
2024-03-24 08:03:38 +0100

    Removed update order line quantities

--- app/models/bill_of_material.rb
+++ app/models/bill_of_material.rb
@@ -4,9 +4,12 @@ class BillOfMaterial < ApplicationRecord
   has_many :components, through: :bill_of_material_lines
   has_many :groups, through: :bill_of_material_lines
   has_many :supplier_orders, dependent: :destroy
+  has_one :client_order, dependent: :destroy
+
   has_many :sales_orders, as: :sourceable
   auto_increment :number, initial: "DIS-#{Date.today.year}-0001"
   has_many :supplier_order_lines, through: :supplier_orders
+
   # Class Methods
 
   # Instance Methods
@@ -34,12 +37,11 @@ class BillOfMaterial < ApplicationRecord
   end
 
   def create_sales_order
-    sales_order = SalesOrder.create(date:, sourceable: self, sendable: Company.find_by(name: 'IMA'),
-                                    receivable: Company.find_by(name: 'TCF'))
+    sales_order = SalesOrder.create!(date:, sourceable: self, sendable: Company.find_by(name: 'IMA'),
+                                     receivable: Company.find_by(name: 'TCF'))
 
     bill_of_material_lines.group(:component_id).sum(:quantity).each do |component_id, quantity|
-      order_line = OrderLine.create!(item_id: component_id, order: sales_order, quantity:)
-      OrderLineQuantity.create(order_line:, total: quantity, ordered: 0, to_order: quantity)
+      sales_order.order_lines.create!(item_id: component_id, quantity:)
     end
   end
 
