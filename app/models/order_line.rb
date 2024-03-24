16713f9b1cf3d9f74c0d622f963229a30bbfdc23
Mirco <mirco.cattabriga@gmail.com>
2024-03-24 08:07:07 +0100

    Clean up

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -3,15 +3,13 @@ class OrderLine < ApplicationRecord
   belongs_to :item
   has_one :order_line_quantity, dependent: :destroy
 
-  after_save :update_ordered_quantities
+  # def update_ordered_quantities
+  #   return unless order.is_a?(PurchaseOrder) && order.sourceable.is_a?(SalesOrder)
 
-  def update_ordered_quantities
-    return unless order.is_a?(PurchaseOrder) && order.sourceable.is_a?(SalesOrder)
+  #   line = order.sourceable.order_lines.find_by(item_id:)
+  #   quantities = line.order_line_quantity
 
-    line = order.sourceable.order_lines.find_by(item_id:)
-    quantities = line.order_line_quantity
-
-    quantities.update!(ordered: quantities.ordered + 1)
-    quantities.update!(to_order: quantities.to_order - 1)
-  end
+  #   quantities.update!(ordered: quantities.ordered + 1)
+  #   quantities.update!(to_order: quantities.to_order - 1)
+  # end
 end
